fa31d9759190d3caa849a82eb9b014806a746173
Bruno Andrade Aguiar Marques <brunoandradeaguiarmarques@hotmail.com>
2023-04-27 22:29:29 -0300

    Modulo Estrutura de Controles

---
+++ CursoCSharp/EstruturasDeControle/UsandoContinue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturasDeControle {
+    internal class UsandoContinue {
+        public static void Executar() {
+
+            int intervalo = 50;
+            Console.WriteLine("Número pares entre 1 e {0}", intervalo);
+
+            for (int i = 1; i <= intervalo; i++) {
+                if (i % intervalo == 1) {
+                    continue;
+                }
+                Console.WriteLine(i + " ");
+            }
+        }
+    }
+}
